169e8c1e4ef889eddedec85a4d33fe8e39c6024b
galactrax <anthonycweathersby@gmail.com>
2020-07-04 14:56:41 -0600

    Update rollup.config

--- source/config/rollup.config.js
+++ source/config/rollup.config.js
@@ -1,19 +1,15 @@
-import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: "./build/library/glow.js",
-    treeshake: true,
+    treeshake: { unknownGlobalSideEffects: true },
     output: [{
-        name: "glow",
-        file: "./build/glow.node.js",
-        format: "cjs",
-        exports: "default"
-    }, {
         name: "glow",
         file: "./build/glow.js",
         format: "iife",
-        exports: "default"
+        exports: "default",
+        globals: { "worker_threads": "null", "os": "null" },
     }],
-    plugins: [commonjs({ include: ['./node_modules/*.*'] }), resolve()]
+    plugins: [resolve({ jail: "", modulesOnly: true })],
+    shimMissingExports: true
 };

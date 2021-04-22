b29d6b91d89eaea6bc5b22cdb60e8695695b22e2
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-04-21 19:34:15 -0600

    Feat: Add AnimationObject type

---
+++ source/typescript/AnimateObject.ts
@@ -0,0 +1,34 @@
+import { t } from "@candlefw/wind/build/types/ascii_code_points";
+import { AnimationInterpolation } from "./types";
+
+export interface AnimationProp {
+
+    [key: number]: AnimationProp,
+    v?: any,
+    value?: any,
+    dur?: number,
+    duration?: number,
+    delay?: number;
+    del?: number;
+    easing?: AnimationInterpolation;
+    e?: AnimationInterpolation;
+    forEach(arg: (arg: AnimationProp) => any): any;
+
+}
+
+export interface AnimationProps {
+    [key: string]: AnimationProp;
+}
+
+export interface AnimateObject {
+    /**
+     * Any object with properties that can be animated.
+     */
+    obj: any;
+
+    match?: any;
+
+    delay?: number;
+
+    [key: string]: AnimationProp;
+}

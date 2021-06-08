ed0db2fcb5a96ceb4489a53ecd40d86c0158b1d5
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-08 17:34:56 -0600

    Break: Remove @candlelib/candle

--- source/typescript/glow.ts
+++ source/typescript/glow.ts
@@ -1,5 +1,3 @@
-import { addModuleToCFW } from "@candlelib/candle";
-
 import { Animation } from "./animation.js";
 import { Transition, Transitioneer } from "./transitioneer.js";
 import { TransformTo } from "./transformto.js";
@@ -8,8 +6,6 @@ Object.assign(Animation, {
 	createTransition: Transitioneer.createTransition,
 	transformTo: TransformTo
 });
-
-addModuleToCFW(Animation, "glow");
 
 export { Transition };
 export default Animation;
